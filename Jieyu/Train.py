cb4ce48535869e9b4ee38f4d31ba0873ad431da2
Jieyu Wang <Jw4937>
2018-11-26 01:33:19 -0500

    Nov26

---
+++ Jieyu/Train.py
@@ -0,0 +1,210 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+from Data_Preprocess import prepareData
+from Encoder import EncoderRNN
+from Decoder import DecoderRNN
+from Attention import LuongAttnDecoderRNN
+from Batch import random_batch
+import string
+import time
+import math
+import torch
+import torch.nn as nn
+import random 
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+
+def train(input_batches, input_lengths, target_batches, target_lengths, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=500):
+    
+    # Zero gradients of both optimizers
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    loss = 0 # Added onto for each word
+
+    # Run words through encoder
+    encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
+    
+    # Prepare input and output variables
+    decoder_input = Variable(torch.LongTensor([SOS_token] * batch_size))
+    decoder_hidden = encoder_hidden[:decoder.n_layers] # Use last (forward) hidden state from encoder
+
+    max_target_length = max(target_lengths)
+    all_decoder_outputs = Variable(torch.zeros(max_target_length, batch_size, decoder.output_size))
+
+    # Move new Variables to CUDA
+    #if USE_CUDA:
+    decoder_input = decoder_input.to(device)
+    all_decoder_outputs = all_decoder_outputs.to(device)
+
+    # Run through decoder one time step at a time
+    for t in range(max_target_length):
+        decoder_output, decoder_hidden, decoder_attn = decoder(
+            decoder_input, decoder_hidden, encoder_outputs
+        )
+
+        all_decoder_outputs[t] = decoder_output
+        decoder_input = target_batches[t] # Next input is current target
+
+    # Loss calculation and backpropagation
+    loss = masked_cross_entropy(
+        all_decoder_outputs.transpose(0, 1).contiguous(), # -> batch x seq
+        target_batches.transpose(0, 1).contiguous(), # -> batch x seq
+        target_lengths
+    )
+    loss.backward()
+    
+#     # Clip gradient norms
+    ec = torch.nn.utils.clip_grad_norm_(encoder.parameters(), clip)
+    dc = torch.nn.utils.clip_grad_norm_(decoder.parameters(), clip)
+
+    # Update parameters with optimizers
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return loss.data.item(), ec, dc
+def as_minutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+def time_since(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+def sequence_mask(sequence_length, max_len=None):
+    if max_len is None:
+        max_len = sequence_length.data.max()
+    batch_size = sequence_length.size(0)
+    seq_range = torch.range(0, max_len - 1).long()
+    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+    seq_range_expand = Variable(seq_range_expand)
+    #if sequence_length.is_cuda:
+    seq_range_expand = seq_range_expand.to(device)
+    seq_length_expand = (sequence_length.unsqueeze(1)
+                         .expand_as(seq_range_expand)).to(device)
+    return seq_range_expand < seq_length_expand
+
+
+def masked_cross_entropy(logits, target, length):
+    length = Variable(torch.LongTensor(length)).to("cpu")
+
+    """
+    Args:
+        logits: A Variable containing a FloatTensor of size
+            (batch, max_len, num_classes) which contains the
+            unnormalized probability for each class.
+        target: A Variable containing a LongTensor of size
+            (batch, max_len) which contains the index of the true
+            class for each corresponding step.
+        length: A Variable containing a LongTensor of size (batch,)
+            which contains the length of each data in a batch.
+
+    Returns:
+        loss: An average loss value masked by the length.
+    """
+
+    # logits_flat: (batch * max_len, num_classes)
+    logits_flat = logits.view(-1, logits.size(-1))
+    # log_probs_flat: (batch * max_len, num_classes)
+    log_probs_flat = F.log_softmax(logits_flat, 1)
+    # target_flat: (batch * max_len, 1)
+    target_flat = target.view(-1, 1)
+    # losses_flat: (batch * max_len, 1)
+    losses_flat = -torch.gather(log_probs_flat, dim=1, index=target_flat)
+    # losses: (batch, max_len)
+    losses = losses_flat.view(*target.size())
+    # mask: (batch, max_len)
+    mask = sequence_mask(sequence_length=length, max_len=target.size(1))
+    losses = losses * mask.float()
+    loss = losses.sum() / length.float().sum().to(device)
+    return loss
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+MAX_LENGTH = 500
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+input_lang, output_lang, pairs = prepareData('zh', 'en', True)
+
+# Configure models
+attn_model = 'dot'
+hidden_size = 500
+n_layers = 2
+dropout = 0.1
+batch_size = 100
+batch_size = 50
+
+# Configure training/optimization
+clip = 50.0
+teacher_forcing_ratio = 0.5
+learning_rate = 0.0001
+decoder_learning_ratio = 5.0
+n_epochs = 50000
+epoch = 0
+plot_every = 20
+print_every = 10
+evaluate_every = 1000
+# encoder && decoder
+encoder = EncoderRNN(input_lang.n_words, hidden_size, n_layers, dropout=dropout)
+decoder = LuongAttnDecoderRNN(attn_model, hidden_size, output_lang.n_words, n_layers, dropout=dropout)
+
+# Initialize optimizers and criterion
+encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
+criterion = nn.CrossEntropyLoss()
+
+# Move models to GPU
+#if USE_CUDA:
+encoder.to(device)
+decoder.to(device)
+start = time.time()
+plot_losses = []
+print_loss_total = 0 # Reset every print_every
+plot_loss_total = 0 # Reset every plot_every
+
+file_loss = open("loss.txt", "w")
+file_log = open("log.txt", "w") 
+
+# Begin!
+ecs = []
+dcs = []
+eca = 0
+dca = 0
+
+while epoch < n_epochs:
+    epoch += 1
+    print(epoch)
+    # Get training data for this cycle
+    input_batches, input_lengths, target_batches, target_lengths = random_batch(batch_size, input_lang, output_lang, pairs)
+
+    # Run the train function
+    loss, ec, dc = train(
+        input_batches, input_lengths, target_batches, target_lengths,
+        encoder, decoder,
+        encoder_optimizer, decoder_optimizer, criterion
+    )
+    
+    file_loss.write(loss, ec, dc)
+    # Keep track of loss
+    print_loss_total += loss
+    plot_loss_total += loss
+    eca += ec
+    dca += dc
+    
+    #job.record(epoch, loss)
+    file.write(print_summary)
+    if epoch % print_every == 0:
+        print_loss_avg = print_loss_total / print_every
+        print_loss_total = 0
+        print_summary = '%s (%d %d%%) %.8f' % (time_since(start, epoch / n_epochs), epoch, (epoch / n_epochs) * 100, print_loss_avg)
+        file_log.write(print_summary)
+        torch.save(encoder.state_dict(), "./model_trained/encoder_{}.pt".format(epoch))
+        torch.save(decoder.state_dict(), "./model_trained/decoder_{}.pt".format(epoch))
+
+file.close()
